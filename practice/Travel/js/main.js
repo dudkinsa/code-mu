3b0cf4a8a127a9d808e29b7e080dbc554044bc20
Dudkin Sergey <dsergik@mail.ru>
2022-12-03 12:47:47 +0300

    Add section with class - destinations  to index.html  Add styles and images. Turn plugin - slick.

---
+++ practice/Travel/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.destination-slider').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        infinite: false,
+        initialSlide:1
+    });
+});
